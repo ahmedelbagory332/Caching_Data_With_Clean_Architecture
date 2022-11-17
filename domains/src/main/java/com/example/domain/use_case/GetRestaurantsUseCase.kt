1fb70055afde7d0131765aca3c924942a64950ab
Ahmed EL Bagory <ahmedelbagory63@gmail.com>
2022-11-17 16:19:11 +0200

    edit on project structure

---
+++ domains/src/main/java/com/example/domain/use_case/GetRestaurantsUseCase.kt
@@ -0,0 +1,28 @@
+package com.example.domain.use_case
+
+import com.example.domain.Resource
+import com.example.domain.RestaurantModel
+import com.example.domain.repository.RestaurantRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetRestaurantsUseCase  @Inject constructor(
+    private val repository: RestaurantRepository,
+
+    ) {
+    operator fun invoke(size:String): Flow<Resource<List<RestaurantModel>>> = flow {
+        try {
+            emit(Resource.Loading<List<RestaurantModel>>())
+               repository.cacheRestaurant(repository.getRemoteRestaurant(size))
+                 repository.getLocalRestaurant().collect {
+                emit(Resource.Success<List<RestaurantModel>>(it))
+               }
+        } catch(e: Exception) {
+            emit(Resource.Error<List<RestaurantModel>>("${e.localizedMessage} : An unexpected error happened"))
+            repository.getLocalRestaurant().collect {
+                emit(Resource.Success<List<RestaurantModel>>(it))
+            }
+        }
+    }
+}
